282f03b55ab76c1e5538dbe3e57b338e9b407b02
nonoperso <154047414+nonoperso@users.noreply.github.com>
2023-12-19 08:18:10 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+	<html lang="fr">
+	<h1 <p style="text-align:left"; font-family: "Arial" </p>     CDs offerts par Nono - Noël 2022 - Lomiquélic (clic sur une pochette pour écouter)</h1>
+	<tr>
+		<th><a href="http://localhost/php-audio-playlist/audio/CD1/audio.php" target="_blank"><img src="img/Recto1.jpg"/> </a></th>
+		<th><a href="http://localhost/php-audio-playlist/audio/CD2/audio.php" target="_blank"><img src="img/Recto2.jpg"/></a></th>
+		<th><a href="http://localhost/php-audio-playlist/audio/CD3/audio.php" target="_blank"><img src="img/Recto3.jpg"/></a></th>
+		<th><a href="http://localhost/php-audio-playlist/audio/CD4/audio.php" target="_blank"><img src="img/Recto4.jpg"/></a></th>
+	</tr>
+	<tr>
+		<th><a href="http://localhost/php-audio-playlist/audio/CD5/audio.php" target="_blank"><img src="img/Recto5.jpg"/></a></th>
+		<th><a href="http://localhost/php-audio-playlist/audio/CD6/audio.php" target="_blank"><img src="img/Recto6.jpg"/></a></th>
+		<th><a href="http://localhost/php-audio-playlist/audio/CD7/audio.php" target="_blank"><img src="img/Recto7.jpg"/></a></th>
+		<th><a href="http://localhost/php-audio-playlist/audio/CD8/audio.php" target="_blank"><img src="img/Recto8.jpg"/></a></th>
+	</tr>	
